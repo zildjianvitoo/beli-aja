ce8aef77f9262f49faf65ecc35f8c3468a8c4b3c
ZildjianVito <zildjianvito07@gmail.com>
2023-10-06 00:53:39 +0700

    refactor: remove trycatch block on get route handlers

--- app/api/color/route.ts
+++ app/api/color/route.ts
@@ -2,14 +2,10 @@ import prisma from "@/app/prismadb";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-  try {
-    const allColors = await prisma.product.findMany({
-      select: {
-        color: true,
-      },
-    });
-    return NextResponse.json(allColors);
-  } catch (error) {
-    return NextResponse.error();
-  }
+  const allColors = await prisma.product.findMany({
+    select: {
+      color: true,
+    },
+  });
+  return NextResponse.json(allColors);
 }
